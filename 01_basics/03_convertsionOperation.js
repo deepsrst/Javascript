cfe031f6b5e7d7453b0c2908f23fe7d92ed28eb3
Deepak Shrestha <deepak.shrestha@mtradeasia.com>
2023-10-15 00:22:09 +0545

    conversionOperations

---
+++ 01_basics/03_convertsionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log("value score : " + score)
+// console.log("typeof score: "+typeof score)
+ 
+let valueInNumber= Number(score)
+// console.log("value ofvalueInNumber : " + valueInNumber)
+// console.log("typeof valueInNumber: "+typeof valueInNumber)
+
+// "33" = > 33 
+// "33abc" => NaN
+// true =>1 , false =>0
+
+
+let isLoggedin = null
+
+// let booleanIsLogedIn  = Boolean(isLoggedin)
+// console.log(booleanIsLogedIn)
+
+//1 => ture, 0 =>false
+//  "" => false
+// "deepak =>  true
+// null , undefined => false
+
+
+let  someNumber= 33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
